59bddbd0691799546b84fcf65fee597a4220d9f7
Michael Hernandez <michaeladrian.hernandez.rala@gmail.com>
2022-12-01 20:47:52 +0100

    Some fixes

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const Types = {
+  REGISTER_USER: "REGISTER_USER",
   LOGIN_USER: "LOGIN_USER",
 };
--- src/components/Dashboard/view/index.js
+++ src/components/Dashboard/view/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "../../Layout/view";
+import Layout from "../../../routes/Layout/view";
 
 import "../styles/index.css";
 
